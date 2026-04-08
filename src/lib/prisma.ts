847c9b21a607b4fa6b6cf44642a135e3a564b539
Oluyemi29 <adedokunoluyemi29@gmail.com>
2026-04-08 20:12:40 +0100

    i update prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+// const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
+
+import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const prisma = new PrismaClient();
 
 export default prisma;
